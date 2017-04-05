8caef19d92da0ed7c7b5601aa0ece3c36a883b6b
wangxiao <wangxiao@ctrip.com>
2017-04-05 19:32:46 +0800

    [M] 重构ReactDOM renderTreeIntoContainer

--- react-lite/src/ReactDOM.js
+++ react-lite/src/ReactDOM.js
@@ -1,124 +1,239 @@
 import * as _ from './util'
 import {
-	COMPONENT_ID,
-	VELEMENT,
-	VCOMPONENT,
-	ELEMENT_NODE_TYPE,
-	DOC_NODE_TYPE,
-	DOCUMENT_FRAGMENT_NODE_TYPE
+  COMPONENT_ID,
+  VELEMENT,
+  VCOMPONENT,
+  ELEMENT_NODE_TYPE,
+  DOC_NODE_TYPE,
+  DOCUMENT_FRAGMENT_NODE_TYPE
 } from './constant'
 import { initVnode, destroyVnode, clearPending, compareTwoVnodes } from './virtual-dom'
 import { updateQueue } from './Component'
 
 function isValidContainer(node) {
-    return !!(node && (
-        node.nodeType === ELEMENT_NODE_TYPE ||
-        node.nodeType === DOC_NODE_TYPE ||
-        node.nodeType === DOCUMENT_FRAGMENT_NODE_TYPE
-    ))
+  return !!(node && (
+    node.nodeType === ELEMENT_NODE_TYPE ||
+    node.nodeType === DOC_NODE_TYPE ||
+    node.nodeType === DOCUMENT_FRAGMENT_NODE_TYPE
+  ))
 }
 
+/**
+ * 
+ */
 let pendingRendering = {}
+
+/**
+ * 存储已渲染过的虚拟节点
+ * @type {Map<VNode>}
+ */
 let vnodeStore = {}
+
+/**
+ * 将vnode渲染到指定的容器元素中
+ * 
+ * @param {any} vnode 
+ * @param {any} container 
+ * @param {any} [callback] 
+ * @param {any} [parentContext] 
+ * @returns 
+ */
 function renderTreeIntoContainer(vnode, container, callback, parentContext) {
-	if (!vnode.vtype) {
-		throw new Error(`cannot render ${ vnode } to container`)
-	}
-	if (!isValidContainer(container)) {
-		throw new Error(`container ${container} is not a DOM element`)
-	}
-	let id = container[COMPONENT_ID] || (container[COMPONENT_ID] = _.getUid())
-	let argsCache = pendingRendering[id]
 
-	// component lify cycle method maybe call root rendering
-	// should bundle them and render by only one time
-	if (argsCache) {
-		if (argsCache === true) {
-			pendingRendering[id] = argsCache = { vnode, callback, parentContext }
-		} else {
-			argsCache.vnode = vnode
-			argsCache.parentContext = parentContext
-			argsCache.callback = argsCache.callback ? _.pipe(argsCache.callback, callback) : callback
-		}
-		return
-	}
+  ensureRenderParamIsValid(vnode, container)
 
-	pendingRendering[id] = true
-	let oldVnode = null
-	let rootNode = null
-	if (oldVnode = vnodeStore[id]) {
-		rootNode = compareTwoVnodes(oldVnode, vnode, container.firstChild, parentContext)
-	} else {
-		rootNode = initVnode(vnode, parentContext, container.namespaceURI)
-		var childNode = null
-		while (childNode = container.lastChild) {
-			container.removeChild(childNode)
-		}
-		container.appendChild(rootNode)
-	}
-	vnodeStore[id] = vnode
-	let isPending = updateQueue.isPending
-	updateQueue.isPending = true
-	clearPending()
-	argsCache = pendingRendering[id]
-	delete pendingRendering[id]
+  let id = getContainerID()
 
-	let result = null
-	if (typeof argsCache === 'object') {
-		result = renderTreeIntoContainer(argsCache.vnode, container, argsCache.callback, argsCache.parentContext)
-	} else if (vnode.vtype === VELEMENT) {
-		result = rootNode
-	} else if (vnode.vtype === VCOMPONENT) {
-		result = rootNode.cache[vnode.uid]
-	}
+  let saveContainerArgument = () => {
+    let argument = { vnode, callback, parentContext }
 
-	if (!isPending) {
-		updateQueue.isPending = false
-		updateQueue.batchUpdate()
-	}
+    return isContainerLocked(id)
+      ? createContainerArgument(id, argument)
+      : updateContainerArgument(id, argument)
+  }
 
-	if (callback) {
-		callback.call(result)
-	}
+  let getContainerArgument = () => getContainerArgument(id)
 
-	return result
+  let argsCache = getContainerArgument()
+
+  let rendered = !!argsCache
+
+  // component lify cycle method maybe call root rendering
+  // should bundle them and render by only one time
+  if (rendered) {
+    saveContainerArgument()
+
+    return
+  }
+
+  lockContainer(id)
+
+  renderDOMTreeToContainer(id, vnode, container, parentContext)
+
+  let isPending = updateQueue.isPending
+  updateQueue.isPending = true
+  clearPending()
+  argsCache = pendingRendering[id]
+
+  unlockContainer(id)
+
+
+
+  let result = null
+
+  if (typeof argsCache === 'object') {
+    result = renderTreeIntoContainer(
+      argsCache.vnode,
+      container,
+      argsCache.callback,
+      argsCache.parentContext)
+  } else if (vnode.vtype === VELEMENT) {
+    result = rootNode
+  } else if (vnode.vtype === VCOMPONENT) {
+    result = rootNode.cache[vnode.uid]
+  }
+
+  if (!isPending) {
+    updateQueue.isPending = false
+    updateQueue.batchUpdate()
+  }
+
+  callCallback(callback, result)
+
+  return result
+}
+
+function ensureRenderParamIsValid(vnode, container) {
+  if (!vnode.vtype) {
+    throw new Error(`cannot render ${vnode} to container`)
+  }
+
+  if (!isValidContainer(container)) {
+    throw new Error(`container ${container} is not a DOM element`)
+  }
+}
+
+function getContainerID(container) {
+  return container[COMPONENT_ID] || (container[COMPONENT_ID] = _.getUid())
+}
+
+function isContainerLocked(id) {
+  return getContainerArgument(id) === true
+}
+
+function lockContainer(id) {
+  pendingRendering[id] = true
+}
+
+function unlockContainer(id) {
+  delete pendingRendering[id]
+}
+
+function getContainerArgument(id) {
+  return pendingRendering[id]
+}
+
+function createContainerArgument(id, argument) {
+  pendingRendering[id] = argument
+
+  return argument
+}
+
+function updateContainerArgument(id, newArgument) {
+  let argument = getContainerArgument(id)
+  let oldCallback = argument.callback
+  let newCallback = newArgument.callback
+
+  argument.vnode = newArgument.vnode
+  argument.parentContext = newArgument.parentContext
+  argument.callback = oldCallback
+    ? _.pipe(oldCallback, newCallback)
+    : newCallback
+
+  pendingRendering[id] = argument
+
+  return argument
+}
+
+function renderDOMTreeToContainer(id, vnode, container, parentContext) {
+  let oldVnode = getVNodeCache(id)
+  let rendered = !!oldVnode
+  let rootNode = null
+
+  rootNode = rendered
+    ? compareTwoVnodes(oldVnode, vnode, container.firstChild, parentContext)
+    : initVnode(vnode, parentContext, container.namespaceURI)
+
+  if (!rendered) {
+    appendToContainer(container, rootNode)
+  }
+
+  setVNodeCache(id, vnode)
+}
+
+function appendToContainer(container, vnode) {
+  clearAllChildNode(container)
+
+  container.appendChild(rootNode)
+}
+
+function clearAllChildNode(node) {
+  var childNode = null
+
+  while (childNode = node.lastChild) {
+    node.removeChild(childNode)
+  }
+}
+
+function getVNodeCache(id) {
+  return vnodeStore[id]
+}
+
+function setVNodeCache(id, vnode) {
+  vnodeStore[id] = vnode
+}
+
+function callCallback(callback, context) {
+  if (callback) {
+    callback.call(context)
+  }
 }
 
 export function render(vnode, container, callback) {
-	return renderTreeIntoContainer(vnode, container, callback)
+  return renderTreeIntoContainer(vnode, container, callback)
 }
 
 export function unstable_renderSubtreeIntoContainer(parentComponent, subVnode, container, callback) {
-	let context = parentComponent.$cache.parentContext
-	return renderTreeIntoContainer(subVnode, container, callback, context)
+  let context = parentComponent.$cache.parentContext
+  return renderTreeIntoContainer(subVnode, container, callback, context)
 }
 
 export function unmountComponentAtNode(container) {
-	if (!container.nodeName) {
-		throw new Error('expect node')
-	}
-	let id = container[COMPONENT_ID]
-	let vnode = null
-	if (vnode = vnodeStore[id]) {
-		destroyVnode(vnode, container.firstChild)
-		container.removeChild(container.firstChild)
-		delete vnodeStore[id]
-		return true
-	}
-	return false
+  if (!container.nodeName) {
+    throw new Error('expect node')
+  }
+  let id = container[COMPONENT_ID]
+  let vnode = null
+  if (vnode = vnodeStore[id]) {
+    destroyVnode(vnode, container.firstChild)
+    container.removeChild(container.firstChild)
+    delete vnodeStore[id]
+    return true
+  }
+  return false
 }
 
 export function findDOMNode(node) {
-	if (node == null) {
-		return null
-	}
-	if (node.nodeName) {
-		return node
-	}
-	let component = node
-	// if component.node equal to false, component must be unmounted
-	if (component.getDOMNode && component.$cache.isMounted) {
-		return component.getDOMNode()
-	}
-	throw new Error('findDOMNode can not find Node')
+  if (node == null) {
+    return null
+  }
+  if (node.nodeName) {
+    return node
+  }
+  let component = node
+  // if component.node equal to false, component must be unmounted
+  if (component.getDOMNode && component.$cache.isMounted) {
+    return component.getDOMNode()
+  }
+  throw new Error('findDOMNode can not find Node')
 }
